d8f74fbe62cd24717e2217f92047f7be8f876187
Anfique Hussain V <90567860+anfiquehussain@users.noreply.github.com>
2024-07-24 18:50:15 +0530

    Update Landing.jsx indent remove

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -10,7 +10,7 @@ function Landing() {
     <div className="flex flex-col items-center h-96 w-full justify-center">
       <h1 className="my-6 text-3xl md:text-6xl font-extrabold drop-shadow-c-drop">ANFIQUE HUSSAIN V</h1>
       <p className="flex space-x-3 md:text-xl text-light">
-        <span className="text-center font-extralight indent-8 ms-2 w-72 md:w-35rem">
+        <span className="text-center font-extralight ms-2 w-72 md:w-35rem">
           <Typewriter
             options={{
               strings: skills,
