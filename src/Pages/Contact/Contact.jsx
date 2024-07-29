9af2dcd304d1192403532268a1708a559c897719
anfique <anfiquehussain1@gmail.com>
2024-07-29 19:24:14 +0530

    address part added to contact page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
 import { HiOutlineMail } from "react-icons/hi";
 import { IoIosContact } from "react-icons/io";
 import { IoLocationOutline } from "react-icons/io5";
-import { FaRegMessage, FaPaperPlane } from "react-icons/fa6";
+import { FaRegMessage, FaPaperPlane, FaEarthAfrica } from "react-icons/fa6";
 import { GrPowerReset } from "react-icons/gr";
+import { FaPhoneAlt } from "react-icons/fa";
 
-const InputField = ({ icon: Icon, type, placeholder, name, value, onChange }) => (
+const InputField = ({
+  icon: Icon,
+  type,
+  placeholder,
+  name,
+  value,
+  onChange,
+}) => (
   <div className="relative mb-4">
     <Icon className="absolute left-3 top-3 text-gray-400" />
     <input
@@ -26,7 +34,7 @@ const Button = ({ type, className, children, onClick, disabled }) => (
     type={type}
     onClick={onClick}
     disabled={disabled}
-    className={`relative flex items-center rounded-lg bg-secondary px-5 py-2 hover:bg-backgroundHover ${className} ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+    className={`relative flex items-center rounded-lg bg-secondary px-5 py-2 hover:bg-backgroundHover ${className} ${disabled ? "cursor-not-allowed opacity-50" : ""}`}
   >
     {children}
   </button>
@@ -37,14 +45,14 @@ export default function Contact() {
     user_email: "",
     user_name: "",
     user_location: "",
-    message: ""
+    message: "",
   });
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [submitStatus, setSubmitStatus] = useState(null);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setFormData(prevData => ({ ...prevData, [name]: value }));
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
   const handleSubmit = async (e) => {
@@ -54,32 +62,45 @@ export default function Contact() {
 
     try {
       const result = await emailjs.send(
-        'service_yyzt3xn',
-        'template_h7hfd8a',
+        "service_yyzt3xn",
+        "template_h7hfd8a",
         formData,
-        '8b6dnsCUFzUHjh-KE'
+        "8b6dnsCUFzUHjh-KE",
       );
 
       console.log(result.text);
-      setSubmitStatus('success');
-      setFormData({ user_email: "", user_name: "", user_location: "", message: "" });
+      setSubmitStatus("success");
+      setFormData({
+        user_email: "",
+        user_name: "",
+        user_location: "",
+        message: "",
+      });
     } catch (error) {
-      console.error('Error sending email:', error);
-      setSubmitStatus('error');
+      console.error("Error sending email:", error);
+      setSubmitStatus("error");
     } finally {
       setIsSubmitting(false);
     }
   };
 
   const handleReset = () => {
-    setFormData({ user_email: "", user_name: "", user_location: "", message: "" });
+    setFormData({
+      user_email: "",
+      user_name: "",
+      user_location: "",
+      message: "",
+    });
     setSubmitStatus(null);
   };
 
   return (
     <div className="container mx-auto p-4">
       <section>
-        <form onSubmit={handleSubmit} className="grid md:gap-4 md:grid-cols-2 lg:mx-40">
+        <form
+          onSubmit={handleSubmit}
+          className="grid md:grid-cols-2 md:gap-4 lg:mx-40"
+        >
           <div className="col-span-1 flex flex-col justify-between md:me-10">
             <InputField
               icon={HiOutlineMail}
@@ -98,14 +119,18 @@ export default function Contact() {
               onChange={handleInputChange}
             />
             <InputField
-              icon={IoLocationOutline}
+              icon={FaEarthAfrica}
               type="text"
               name="user_location"
-              placeholder="Location"
+              placeholder="Location/County"
               value={formData.user_location}
               onChange={handleInputChange}
             />
-            <Button type="button" onClick={handleReset} className="hidden w-fit self-end md:flex">
+            <Button
+              type="button"
+              onClick={handleReset}
+              className="hidden w-fit self-end md:flex"
+            >
               <GrPowerReset className="mr-2" />
               Reset
             </Button>
@@ -124,23 +149,75 @@ export default function Contact() {
               ></textarea>
             </div>
             <div className="flex justify-between">
-              <Button type="submit" className="w-full md:w-fit" disabled={isSubmitting}>
+              <Button
+                type="submit"
+                className="w-full md:w-fit"
+                disabled={isSubmitting}
+              >
                 <FaPaperPlane className="mr-2" />
-                {isSubmitting ? 'Sending...' : 'Submit'}
+                {isSubmitting ? "Sending..." : "Sent"}
               </Button>
-              <Button type="button" onClick={handleReset} className="ms-2 w-full md:hidden">
+              <Button
+                type="button"
+                onClick={handleReset}
+                className="ms-2 w-full md:hidden"
+              >
                 <GrPowerReset className="mr-2" />
                 Reset
               </Button>
             </div>
           </div>
         </form>
-        {submitStatus === 'success' && (
+        {submitStatus === "success" && (
           <p className="mt-4 text-green-500">Email sent successfully!</p>
         )}
-        {submitStatus === 'error' && (
-          <p className="mt-4 text-red-500">Error sending email. Please try again.</p>
+        {submitStatus === "error" && (
+          <p className="mt-4 text-red-500">
+            Error sending email. Please try again.
+          </p>
         )}
+      </section>
+
+      <section>
+        <div className="grid md:grid-cols-2 mt-10 md:mt-0 md:gap-4 lg:mx-40">
+          <div>
+            <div className="relative text-white gap-2 text-3xl md:text-xl flex items-center md:mb-0 mb-3">
+            <IoLocationOutline /><h1>Address</h1> 
+            </div>
+            <address className="text-light my-2 md:text-sm font-mono text-base">
+              Veyattummal <br />
+              Vattapparapoyil <br />
+              Narikkuni(PO),673585 <br />
+              Kozhikode,Kerala <br />
+              INDIA <br />
+            </address>
+            <div className="relative text-white gap-2 text-3xl md:text-xl flex items-center md:mb-0 mb-3">
+              <FaPhoneAlt /><h1>Phone</h1>
+            </div>
+            <p className="text-light my-2 md:text-sm font-mono text-base">
+              +91 9995424875
+            </p>
+
+            <div className="relative text-white gap-2 text-3xl md:text-xl flex items-center md:mb-0 mb-3">
+              <IoLocationOutline /><h1>Email</h1>
+            </div>
+            <a href="mailto:anfiquehussain6@gmail.com" className="text-light my-2 md:text-sm font-mono text-base"> 
+              anfiquehussain6@gmail.com
+            </a>
+            <br />
+            <a href="mailto:adomax2003@gmail.com" className="text-light my-2 md:text-sm font-mono text-base">
+              adomax2003@gmail.com
+            </a>
+
+
+          </div>
+
+          <div>
+            
+          </div>
+
+
+        </div>
       </section>
     </div>
   );
