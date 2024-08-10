9e88ee8be00159f56f2cc5ca3aaacd2d162969f8
anfiquehussain <adomax2003@gmail.com>
2024-08-10 12:52:55 +0530

    fix version typo

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <BrowserRouter>
       <div className="bg-primary text-white">
         <span className="opacity-4 fixed bottom-0 right-0 m-4 font-mono text-xs text-gray-700">
-          <Link to='/v'>v1.1</Link>
+          <Link to='/v'>v1.2</Link>
         </span>
         <Navbar />
         <Routes>
